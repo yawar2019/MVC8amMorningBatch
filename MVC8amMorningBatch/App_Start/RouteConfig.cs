ea0e048278f45372a8c737c68e16f6f68a4385d8
yawar2019 <yawarali17@gmail.com>
2020-06-23 09:06:55 +0530

    Added Project MVC 8 am batch

--- MVC8amMorningBatch/App_Start/RouteConfig.cs
+++ MVC8amMorningBatch/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC8amMorningBatch
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, name = UrlParameter.Optional }
             );
         }
     }
---
+++ MVC8amMorningBatch/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using MVC8amMorningBatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC8amMorningBatch.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Get() {
+
+            return View();
+        }
+
+        public string Index()
+        {
+            return "hello world";
+        }
+
+        public int Index1(int id)
+        {
+            return  id;
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public string GetData(int? id,string name)
+        {
+
+            return "Employee Id: "+ id+" Name: "+name;   
+
+        }
+
+        public string GetData2(int? id)
+        {
+            string Name = Request.QueryString["name"];
+
+            return "Employee Id: " + id + "Name: "+ Name;
+
+        }
+
+        public ActionResult GetLuckyData() {
+            
+            ViewBag.Lucky = 1;
+            return View("GetData");
+        }
+
+        public ActionResult GetLuckyPersonData()
+        {
+            List<Employee> listObj = new List<Employee>();
+            Employee objRadhika = new Employee();
+            objRadhika.EmpId = 1;
+            objRadhika.EmpName = "Harini";
+            objRadhika.EmpSalary = 210000;
+
+            Employee objShiva = new Employee();
+            objShiva.EmpId = 2;
+            objShiva.EmpName = "Shiva";
+            objShiva.EmpSalary = 420000;
+
+            listObj.Add(objRadhika);
+            listObj.Add(objShiva);
+
+
+            ViewBag.Lucky = listObj;
+
+
+
+
+            return View("GetData");
+        }
+
+    }
+}
